5b20e0d49d72d7dbcacfc0f24d468b6bf276ab84
redknight517 <22489486@qq.com>
2017-09-28 17:17:31 +0800

    code the two CNN layers

--- 5_3_CNN_Cifar10/5_3_Cifar10.py
+++ 5_3_CNN_Cifar10/5_3_Cifar10.py
@@ -17,11 +17,36 @@ def variable_with_weight_loss(shape, stddev, wl):
     return var
 
 
+# download cifar10 data
+cifar10.maybe_download_and_extract()
+
+# create the train image, train image will be do some data augmentation
 images_train, labels_train = cifar10_input.distorted_inputs(data_dir=data_dir, batch_size=batch_size)
 
+# create the test image
 images_test, labels_test = cifar10_input.inputs(eval_data=True, data_dir=data_dir, batch_size=batch_size)
 
+# Placeholder for image and label
+image_holder = tf.placeholder(tf.float32, [batch_size, 24, 24, 3])
+label_holder = tf.placeholder(tf.float32, [batch_size])
 
+# first CNN layer
 
+# 64 convolutional core, 3 color path, 5x5 size.
+weight1 = variable_with_weight_loss(shape=[5,5,3,64], stddev=0.05, wl=0.0)
+kernel1 = tf.nn.conv2d(image_holder, weight1, [1,1,1,1], padding='SAME')
+bias1 = tf.Variable(tf.constant(0.0, shape=[64]))
+conv1 = tf.nn.relu(tf.nn.bias_add(kernel1,bias1))
+pool1 = tf.nn.max_pool(conv1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
+norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
 
-cifar10.maybe_download_and_extract()
+# second CNN layer
+
+# 64 core, 64 path, 5x5 size
+# change the sequence about pool and lrn
+weight2 = variable_with_weight_loss(shape=[5,5,64,64],stddev=0.05, wl=0.0)
+kernel2 = tf.nn.conv2d(norm1, weight2, [1,1,1,1], padding='SAME')
+bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
+conv2 = tf.nn.relu(tf.nn.bias_add(kernel2, bias2))
+norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001/9.1, beta=0.75)
+pool2 = tf.nn.max_pool(norm2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
