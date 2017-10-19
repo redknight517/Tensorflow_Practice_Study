37c983c9911e540e851f9bd86fe585fd589f03c2
redknight517 <22489486@qq.com>
2017-10-19 22:11:48 +0800

    add two connect layers

--- 5_3_CNN_Cifar10/5_3_Cifar10.py
+++ 5_3_CNN_Cifar10/5_3_Cifar10.py
@@ -50,3 +50,17 @@ bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
 conv2 = tf.nn.relu(tf.nn.bias_add(kernel2, bias2))
 norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001/9.1, beta=0.75)
 pool2 = tf.nn.max_pool(norm2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
+
+# first full connect layer
+reshape = tf.reshape(pool2, [batch_size, -1])
+dim = reshape.get_shape()[1].value
+weight3 = variable_with_weight_loss(shape[dim,384], stddev=0.04, wl=0.004)
+bias3 = tf.Variable(tf.constant(0.1, shape=[384]))
+local3 = tf.nn.relu(tf.matmul(reshape, weight3) + bias3)
+
+# second full connect layer
+weight4 = variable_with_weight_loss(shape=[384, 192], stddev=0.04, wl=0.004)
+bias4 = tf.Variable(tf.constant(0.1, shape=[192]))
+local4 = tf.nn.relu(tf.matmul(local3, weight4) + bias4)
+
+# last layer
